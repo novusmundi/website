84e39804cdfbba7beea9c4b3dc8662557f651198
w0xter <lpozogilo@gmail.com>
2021-10-27 22:45:01 +0200

    including newsletter

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,35 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+function validateAndSendform(){
+    const email = document.getElementById("email").value
+    const valid = RegExp( /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(email)
+    const alert = document.getElementById("alert")
+    if(valid){
+        axios.post("https://trib3.app/newsletter",{email:email}).then((response) => {
+            
+            if(response && response.data){
+                console.log(response.data)
+                alert.innerHTML = `
+                    <p class="text-white">
+                        ${response.data}
+                    </p>
+                `
+            }
+        }).catch((err) => {
+            if(err && err.response && err.response.data){
+                alert.innerHTML = `
+                <p class="text-warning">
+                    ${err.response.data}
+                </p>
+              `
+            }
+        })
+    }else{
+        alert.innerHTML = `
+        <p class="text-warning">
+            This is not a valid email. 
+        </p>
+      `    }
+}
